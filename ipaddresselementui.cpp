d85dfbdde5a635ca1a6c550dd10cf1b580f11114
Adrian Suciu <adrian.suciu@analog.com>
2017-08-09 17:22:23 +0300

    Prevent crash when read command returns empty string for IpAddressElement
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- ipaddresselementui.cpp
+++ ipaddresselementui.cpp
@@ -68,10 +68,20 @@ void IPAddressElement::formatForReading()
 void IPAddressElement::strToIp()
 {
 	QStringList ipList = ipStr.split(".");
-	ip[3] = ipList.at(0).toInt();
-	ip[2] = ipList.at(1).toInt();
-	ip[1] = ipList.at(2).toInt();;
-	ip[0] = ipList.at(3).toInt();;
+	if(ipList.count()==4)
+	{
+		ip[3] = ipList.at(0).toInt();
+		ip[2] = ipList.at(1).toInt();
+		ip[1] = ipList.at(2).toInt();;
+		ip[0] = ipList.at(3).toInt();;
+	}
+	else
+	{
+		ip[3] = 0;
+		ip[2] = 0;
+		ip[1] = 0;
+		ip[0] = 0;
+	}
 }
 
 void IPAddressElement::update()

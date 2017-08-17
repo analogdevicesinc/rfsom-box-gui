d9fa8e6a117cebb319973dec12aad779666a8ead
Michael Hennerich <michael.hennerich@analog.com>
2017-08-17 15:01:51 +0200

    bin/rfsom-box-gui-start.sh: execute only on RFSOM-BOX
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- bin/rfsom-box-gui-start.sh
+++ bin/rfsom-box-gui-start.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-sudo service lightdm stop
-QT_QPA_EVDEV_KEYBOARD_PARAMETERS=/dev/input/by-path/platform-gpio-keys-nav-switch-event:grab=1 
-QT_QPA_EVDEV_MOUSE_PARAMETERS=/dev/input/by-path/platform-rotary-event:grab=1 
-QT_QPA_FB_DRM=1
-QT_QPA_FB_HIDECURSOR=0
-QT_QPA_FB_TSLIB=1
-QT_QPA_FONTDIR=/home/analog/Qt/fonts
-QT_QPA_PLATFORM=linuxfb
-QT_QPA_GENERIC_PLUGINS=evdevmouse,evdevkeyboard
-rfsom-box-gui 
+grep -q "RFSOM-BOX" /sys/firmware/devicetree/base/model
+if [ $? -eq 0 ]; then
+	sudo service lightdm stop;
+	QT_QPA_EVDEV_KEYBOARD_PARAMETERS=/dev/input/by-path/platform-gpio-keys-nav-switch-event:grab=1 \
+	QT_QPA_EVDEV_MOUSE_PARAMETERS=/dev/input/by-path/platform-rotary-event:grab=1 \
+	QT_QPA_FB_DRM=1 \
+	QT_QPA_FB_HIDECURSOR=0 \
+	QT_QPA_FB_TSLIB=1 \
+	QT_QPA_FONTDIR=/home/analog/Qt/fonts \
+	QT_QPA_PLATFORM=linuxfb \
+	QT_QPA_GENERIC_PLUGINS=evdevmouse,evdevkeyboard \
+	rfsom-box-gui
+fi

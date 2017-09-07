16a0ccbbf9a1f4a31cf109b4d603481fa2f3d4c0
MacLean <Erik.MacLean@analog.com>
2017-09-07 14:50:23 -0400

    fixed kill_modem.sh

--- tun_tap/kill_modem.sh
+++ tun_tap/kill_modem.sh
@@ -2,14 +2,6 @@
 
 PID=$(pidof modemd)
 kill -9 $PID
-sh cfg_modem.sh
-./modemd &
-./en_macsec.sh
-root@analog:/home/analog/tap1# cat kill_modem.sh
-#!/bin/bash
-
-PID=$(pidof modemd)
-kill -9 $PID
 
 PID=$(pidof wpa_supplicant)
 kill -9 $PID

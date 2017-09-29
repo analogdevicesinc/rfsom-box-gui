46b2250061ba9dcf55d5c3321359b51b0e530582
Adrian Suciu <adrian.suciu@analog.com>
2017-09-29 11:12:19 +0300

    launcher.json - don't use /dev/video0 by default
    Finds first video device by listing /dev/video* and using that device.
    
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- bin/start_stream.sh
+++ bin/start_stream.sh
@@ -5,5 +5,6 @@
 ip=$(cat /usr/local/etc/rfsom-box-gui/stream-ip)
 port=$(cat /usr/local/etc/rfsom-box-gui/modem-port)
 udp=$(cat /usr/local/etc/rfsom-box-gui/netcat-param)
-/usr/local/bin/ffmpeg -f v4l2 -framerate 20 -video_size 320x240 -i /dev/video0 -r 20 -s 160x128 -movflags faststart -an -vcodec libx264 -preset superfast -tune zerolatency -f avi pipe:1 | netcat $udp $ip $port 
+video=$(ls /dev/video* | head -n1 | cut -d " " -f1)
+/usr/local/bin/ffmpeg -f v4l2 -framerate 20 -video_size 320x240 -i $video -r 20 -s 160x128 -movflags faststart -an -vcodec libx264 -preset superfast -tune zerolatency -f avi pipe:1 | netcat $udp $ip $port 
 kill $fimpid

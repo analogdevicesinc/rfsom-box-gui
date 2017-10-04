f652bdff977781a6710c962ae32108d3533160bb
Adrian Suciu <adrian.suciu@analog.com>
2017-10-04 17:32:02 +0300

    Fix camera view video size
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- bin/camera_view.sh
+++ bin/camera_view.sh
@@ -1,7 +1,7 @@
 camnr=$(ls /dev -1 | grep video | wc -l)
 
 if [ $camnr -gt 0 ]; then
-mplayer -vf scale -zoom -xy 160 -nosound -vo fbdev -tv driver=v4l2:device=$(ls /dev/video* | head -n1 | cut -d " " -f1) tv://;
+mplayer -vf scale -zoom -x 160 -y 128 -nosound -vo fbdev -tv driver=v4l2:device=$(ls /dev/video* | head -n1 | cut -d " " -f1) tv:// 2>/dev/null
 else
 (>&2 echo "No webcam found");
 exit 1;

2c50d5bd0df76d36dedbf5646ad9f906d18bbef0
Robin Getz <robin.getz@analog.com>
2018-02-24 00:07:53 +0000

    batt_man.sh : fix some things that were causing some shell issues with 	the version of bash that is on the target. 	Make a little more robust, and error/warn when running into a problem. 	Fix spelling.
    Signed-off-by: Robin Getz <robin.getz@analog.com>

--- bin/batt_man.sh
+++ bin/batt_man.sh
@@ -2,15 +2,42 @@
 
 dev=/sys/class/power_supply/ltc2942
 
+if [ ! -f $dev/voltage_now ] ; then
+	echo ltc2942 not installed
+	exit 1
+fi
+
+if [ "$(id -u)" != "0" ] ; then
+        echo "This script must be run as root"
+        exit 1
+fi
+
+if [ $(i2cdetect -y 0 0x14 0x14 | grep -e "^10:" | grep 14 | wc -l) -eq "0" ] ; then
+	echo "Could not find ADP5061"
+	exit 1
+fi
+
+me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+
+for pid in $(pidof -x ${me}); do
+	if [ ${pid} != $$ ]; then
+		echo killing duplicate process ${pid}
+		kill -9 ${pid}
+	fi
+done
+
+previous_mode=foo
 while true; do
-	voltage=`echo "scale=5;$(<$dev/voltage_now) / 1000000" | bc`
+	voltage=`echo "scale=5;$(cat $dev/voltage_now) / 1000000" | bc`
+	# Get the ADP5061 Linear Battery Charger status
+	# this is a super dirty hack, since a driver doesn't exist yet.
 	status=`i2cget -y 0 0x14 0xB`
 
 	case $(($status & 0x7)) in
 		0)  mode="Off" ;;
 		1)  mode="Trickle Charge" ;;
 		2)  mode="Fast Charge Constant Current" ;;
-		3)  mode="Fast Charge Constant Volatge" ;;
+		3)  mode="Fast Charge Constant Voltage" ;;
 		4)  mode="Charge Complete" ;;
 		5)  mode="LDO mode" ;;
 		6)  mode="Charge Timer Expired" ;;
@@ -24,17 +51,19 @@ while true; do
 
 	#echo $voltage $mode $soc%
 
-	echo "$voltage" > /tmp/rfsom_batt_volatge
+	echo "$voltage" > /tmp/rfsom_batt_voltage
 	echo "$soc%" > /tmp/rfsom_batt_soc
 	echo "$mode" > /tmp/rfsom_batt_mode
 
-	if [ "$mode" == "Charge Complete" ] && [ "$previous_mode" == "Fast Charge Constant Volatge" ]; then
+	if [ "$mode" = "Charge Complete" -a \
+	     "$previous_mode" = "Fast Charge Constant Voltage" ]; then
 		echo 3200000 > $dev/charge_now
 	fi
 
 	previous_mode=$mode
 
-	if [ $(echo "$voltage < 3.3" | bc -l) == "1" ] && [ "$mode" == "Off" ]; then
+	if [ $(echo "$voltage < 3.3" | bc -l) -eq "1" -a \
+	     "$mode" = "Off" ]; then
 		echo 0 > $dev/charge_now
 		poweroff
 	fi

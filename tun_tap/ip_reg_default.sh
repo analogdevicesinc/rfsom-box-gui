56fb6733e7ca6b452f40e21f17d5cb29c6be0f63
Travis Collins <travis.collins@analog.com>
2018-10-15 20:29:37 -0400

    Fix default register settings for modem
    Signed-off-by: Travis Collins <travis.collins@analog.com>

--- tun_tap/ip_reg_default.sh
+++ tun_tap/ip_reg_default.sh
@@ -1,6 +1,6 @@
 /usr/local/bin/ip_reg -x
 echo 40 > /tmp/ip_modem_reg_100
-echo 50 > /tmp/ip_modem_reg_104
+echo 200 > /tmp/ip_modem_reg_104
 echo 2  > /tmp/ip_modem_reg_108
 echo 0 > /tmp/ip_modem_reg_10c
 echo 0 > /tmp/ip_modem_reg_110
@@ -10,4 +10,5 @@ echo 10 > /tmp/ip_modem_reg_11c
 echo 0 > /tmp/ip_modem_reg_120
 echo 0 > /tmp/ip_modem_reg_124
 echo 0 > /tmp/ip_modem_reg_128
-echo 0 > /tmp/ip_modem_reg_12c
+echo 1 > /tmp/ip_modem_reg_12c
+echo 0 > /tmp/ip_modem_reg_130

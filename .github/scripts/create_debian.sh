f3ef4323524ee9a52a8b800a8d8537099b7cc1a9
Nicu Siderias <nicu.siderias@analog.com>
2025-11-20 10:37:22 +0200

    Enhance dependency
    Delete dependencies that are listed from dpkg
    
    Install dependency with apt to verify the application
    
    Signed-off-by: Nicu Siderias <nicu.siderias@analog.com>

--- .github/scripts/create_debian.sh
+++ .github/scripts/create_debian.sh
@@ -7,7 +7,7 @@ source_code=$(basename "$PWD")
 
 # Install build dependencies
 sudo apt update
-sudo apt install -y build-essential make devscripts debhelper qtbase5-dev qt5-qmake cmake libfftw3-dev
+sudo apt install -y build-essential make devscripts debhelper qtbase5-dev qt5-qmake cmake libfftw3-dev gpsd gpsd-clients bc
 
 # Replace placeholders inside the debian template files
 sed -i "s/@VERSION@/$version-1/" packaging/debian/changelog
